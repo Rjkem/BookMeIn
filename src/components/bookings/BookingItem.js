72cdfb62bc6fb3fba55687d65cf9bef2eb104c79
PsyChrome-G3 <michael.ashdown@g3cko-design.uk>
2021-07-24 23:22:41 +0100

    Created the Card wrapper for individual bookings.

--- src/components/bookings/BookingItem.js
+++ src/components/bookings/BookingItem.js
@@ -1,24 +1,27 @@
 // Import local files
+import Card from "../ui/Card";
 import classes from "./BookingItem.module.css";
 
 function BookingItem(props) {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.session} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.session}</h3>
-        <date>{props.date}</date>
-        <time>
-          {props.start_time} - {props.finish_time}
-        </time>
-        <category>{props.category}</category>
-        <attendees>{props.attendees}</attendees>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favourites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.session} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.session}</h3>
+          <date>{props.date}</date>
+          <time>
+            {props.start_time} - {props.finish_time}
+          </time>
+          <category>{props.category}</category>
+          <attendees>{props.attendees}</attendees>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favourites</button>
+        </div>
+      </Card>
     </li>
   );
 }
