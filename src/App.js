c342b9584b7b765064a85735e1b456d4e08c6f7f
PsyChrome-G3 <michael.ashdown@g3cko-design.uk>
2021-07-25 20:41:45 +0100

    Adjusted the styling of the cards.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import { Route, Switch } from "react-router-dom";
 import DashboardPage from "./pages/Dashboard";
 import FavouritesPage from "./pages/Favourites";
 import NewBookingPage from "./pages/NewBooking";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      {/* Show the main navigation bar at the top. */}
-      <MainNavigation />
-      {/* Routes */}
-      <Switch>
-        <Route path="/" exact>
-          <DashboardPage />
-        </Route>
-        <Route path="/new-booking">
-          <NewBookingPage />
-        </Route>
-        <Route path="/favourites">
-          <FavouritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        {/* Routes */}
+        <Switch>
+          <Route path="/" exact>
+            <DashboardPage />
+          </Route>
+          <Route path="/new-booking">
+            <NewBookingPage />
+          </Route>
+          <Route path="/favourites">
+            <FavouritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
