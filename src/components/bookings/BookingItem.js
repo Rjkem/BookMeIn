0d3f4ceb44504d7898f03ceed848645a0f1b2aab
PsyChrome-G3 <michael.ashdown@g3cko-design.uk>
2021-07-26 22:27:08 +0100

    Modified the favourites page to show volatile favourites.

--- src/components/bookings/BookingItem.js
+++ src/components/bookings/BookingItem.js
@@ -1,8 +1,34 @@
+// Third party imports
+import { useContext } from 'react';
+
 // Import local files
 import Card from "../ui/Card";
 import classes from "./BookingItem.module.css";
+import FavouritesContext from '../../store/favourites-context';
 
 function BookingItem(props) {
+  const favouritesCtx = useContext(FavouritesContext);
+
+  const sessionIsFavourite = favouritesCtx.sessionIsFavourite(props.id);
+
+  function toggleFavouriteStatusHandler() {
+    if (sessionIsFavourite) {
+      favouritesCtx.removeFavourite(props.id);
+    } else {
+      favouritesCtx.addFavourite({
+        id: props.id,
+        category: props.category,
+        sub_category: props.sub_category,
+        title: props.title,
+        date: props.date,
+        start_time: props.start_time,
+        finish_time: props.finish_time,
+        max_attendees: props.max_attendees,
+        image: props.image
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -19,7 +45,7 @@ function BookingItem(props) {
           <attendees>Attending: 0 / {props.max_attendees}</attendees>
         </div>
         <div className={classes.actions}>
-          <button>To Favourites</button>
+          <button onClick={toggleFavouriteStatusHandler}>{sessionIsFavourite ? 'Remove from Favourites' : 'Add to Favourites'}</button>
         </div>
       </Card>
     </li>
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,25 @@
+// Third party imports
+import { useContext } from 'react';
+
+// Local imports
+import FavouritesContext from '../store/favourites-context';
+import BookingList from '../components/bookings/BookingList';
+
 function FavouritesPage() {
-  return <div>Favourites Page</div>;
+  const favouritesCtx = useContext(FavouritesContext);
+
+  let content;
+
+  if (favouritesCtx.totalFavourites === 0) {
+    content = <p>You have not added any sessions as favourites yet.</p>
+  } else {
+    content = <BookingList bookings={favouritesCtx.favourites} />
+  }
+
+  return <section>
+    <h1>My Favourites</h1>
+    {content}
+  </section>;
 }
 
 export default FavouritesPage;
